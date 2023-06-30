2a7ea9ebe2e1e5cb0516e01e3220d510e8f672b1
ingonzal <zen42.ikas@gmail.com>
2023-06-30 20:02:05 +0200

    CPP06, ex02 DONE

--- cursus/6Ring/cpp/cpp06/ex02/inc/serialization.hpp
+++ cursus/6Ring/cpp/cpp06/ex02/inc/serialization.hpp
@@ -6,7 +6,7 @@
 /*   By: ingonzal <ingonzal@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 20:03:47 by ingonzal          #+#    #+#             */
-/*   Updated: 2023/06/29 20:20:13 by ingonzal         ###   ########.fr       */
+/*   Updated: 2023/06/30 20:01:25 by ingonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ class A : public Base {};
 class B : public Base {};
 class C : public Base {};
 
-/* std::ostream & operator<<(std::ostream & o, Base const & rhs); */
+Base * generate(void);
+void identify(Base* p);
+void identify(Base& p);
 
 #endif
--- cursus/6Ring/cpp/cpp06/ex02/main.cpp
+++ cursus/6Ring/cpp/cpp06/ex02/main.cpp
@@ -6,43 +6,21 @@
 /*   By: ingonzal <ingonzal@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 18:16:33 by ingonzal          #+#    #+#             */
-/*   Updated: 2023/06/29 20:06:31 by ingonzal         ###   ########.fr       */
+/*   Updated: 2023/06/30 20:01:19 by ingonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "serialization.hpp"
-#include <iostream>
-#include <ctype.h>
-#include <string.h>
 
 int	main(void)
 {
-	/* Serializer::Data data; */
-	/* data.num = 4; */
-	/* data.chars = 'a'; */
+	Base *basePtr;
 
-	/* uintptr_t			ptr; */
-	/* Serializer::Data	result; */
+	basePtr = generate();
+	Base &baseRef = *basePtr;
+	identify(basePtr);
+	identify(baseRef);
 
-	/* std::cout << "Init struct: " << std::endl; */
-    /* std::cout << data << std::endl; */
-
-	/* std::cout << "Serialize: " << std::endl; */
-	/* ptr = Serializer::serialize(&data); */
-
-	/* std::cout << "Serialized struct: " << ptr << std::endl; */
-	/* std::cout << "Memory allocation: " << &ptr << std::endl; */
-	/* std::cout << std::endl; */
-
-	/* std::cout << "Deserialize struct: " << std::endl; */
-	/* result = *Serializer::deserialize(ptr); */
-
-    /* std::cout << result << std::endl; */
-
-
-	/* std::cout << "Change result Data: " << std::endl; */
-	/* result.num = 5; */
-	/* result.chars = 'b'; */
-    /* std::cout << result << std::endl; */
-	/* return (0); */
+	delete(basePtr);
+	return (0);
 }
