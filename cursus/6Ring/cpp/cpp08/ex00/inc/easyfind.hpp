1369f66214f8b06f27d15a261275516ba7a6cc0c
ingonzal <zen42.ikas@gmail.com>
2023-07-14 18:06:27 +0200

    cpp08_EX00_DONE

--- cursus/6Ring/cpp/cpp08/ex00/inc/easyfind.hpp
+++ cursus/6Ring/cpp/cpp08/ex00/inc/easyfind.hpp
@@ -6,32 +6,35 @@
 /*   By: ingonzal <ingonzal@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 18:45:56 by ingonzal          #+#    #+#             */
-/*   Updated: 2023/07/13 18:56:24 by ingonzal         ###   ########.fr       */
+/*   Updated: 2023/07/14 18:05:38 by ingonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# define RESET		"\033[0m"
+# define RED		"\033[31m"
+# define GREEN		"\033[32m"
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <exception>
 
 template<typename T>
-int easyfind(std::vector<T>vec, T x){
+void easyfind(std::vector<T>vec, int x){
 	
 	typename std::vector<T>::iterator it;
 
 	it = std::find(vec.begin(), vec.end(), x);
 	if (it != vec.end()){
-		std::cout << "Element " << x <<
+		std::cout << GREEN << "Element " << x <<
 					 " found at position : ";
-		std::cout << it - vec.begin() + 1 << std::endl;
-		return (0);
+		std::cout << it - vec.begin() + 1 << RESET << std::endl;
 	  }
    	else
-		throw ("kkkkk");
-	return (0);
+		throw std::runtime_error("Value not found in container");
 }
 
 
