e9d2ba049735724e3038246a60fc21a2d372b69f
ingonzal <zen42.ikas@gmail.com>
2023-07-20 19:03:56 +0200

    cpp08_EX01 autoFill pending

--- cursus/6Ring/cpp/cpp08/ex01/main.cpp
+++ cursus/6Ring/cpp/cpp08/ex01/main.cpp
@@ -6,62 +6,73 @@
 /*   By: ingonzal <ingonzal@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 14:24:30 by ingonzal          #+#    #+#             */
-/*   Updated: 2023/07/19 19:57:30 by ingonzal         ###   ########.fr       */
+/*   Updated: 2023/07/20 19:03:17 by ingonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
 int main(void){
-	Span a(6);
+	Span a(5);
 
 	try{
-		a.addNumber(2);
+		a.addNumber(6);
 	}
 	catch(std::runtime_error &e){
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RESET << std::endl;
 	}
 	try{
 		a.addNumber(3);
 	}
 	catch(std::runtime_error &e){
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RESET << std::endl;
 	}
 	try{
-		a.addNumber(4);
+		a.addNumber(17);
 	}
 	catch(std::runtime_error &e){
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RESET << std::endl;
 	}
 	try{
-		a.addNumber(5);
+		a.addNumber(9);
 	}
 	catch(std::runtime_error &e){
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RESET << std::endl;
 	}
 	try{
-		a.addNumber(6);
+		a.addNumber(11);
 	}
 	catch(std::runtime_error &e){
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RESET << std::endl;
 	}
 	try{
-		a.addNumber(7);
+		a.addNumber(16);
 	}
 	catch(std::runtime_error &e){
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RESET << std::endl;
 	}
 	try{
-		a.addNumber(8);
+		a.addNumber(19);
 	}
 	catch(std::runtime_error &e){
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RESET << std::endl;
 	}
+
 	Span b(a);
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
-	std::cout << a.longestSpan() << std::endl;
-	std::cout << a.shortestSpan() << std::endl;
 
+	try{
+		std::cout << a.shortestSpan() << std::endl;
+	}
+	catch(std::runtime_error &e){
+		std::cout << RED << e.what() << RESET << std::endl;
+	}
+	try{
+		std::cout << a.longestSpan() << std::endl;
+	}
+	catch(std::runtime_error &e){
+		std::cout << RED << e.what() << RESET << std::endl;
+	}
 	return (0);
 }
--- cursus/6Ring/cpp/cpp08/ex01/src/Span.cpp
+++ cursus/6Ring/cpp/cpp08/ex01/src/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: ingonzal <ingonzal@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 17:56:09 by ingonzal          #+#    #+#             */
-/*   Updated: 2023/07/19 20:37:58 by ingonzal         ###   ########.fr       */
+/*   Updated: 2023/07/20 19:02:22 by ingonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ Span::Span(void) : _N(0){
 }
 
 Span::Span(unsigned int N) : _N(N), _vec(new std::vector<int>()){
-		/* this->_vec->reserve(_N); */
+		this->_vec->reserve(_N);
 }
 
 Span::Span(Span const & src){
@@ -49,7 +49,7 @@ std::vector<int> & Span::getVec(void) const{
 }
 
 void Span::addNumber(int N){
-	std::cout << this->getVec().size() << std::endl;
+	/* std::cout << this->getVec().size() << std::endl; */
 	if (this->getN() > this->getVec().size())
 		this->_vec->push_back(N);
 	else 
@@ -57,6 +57,8 @@ void Span::addNumber(int N){
 }
 
 int Span::longestSpan(){
+	if(this->_vec->size() < 2) 
+		throw std::runtime_error("Not enough vector size");
 	int max;
 	int min;
 	max = this->_vec->at(0); 
@@ -71,28 +73,31 @@ int Span::longestSpan(){
 }
 
 int Span::shortestSpan(){
+	if(this->_vec->size() < 2) 
+		throw std::runtime_error("Not enough vector size");
+	int tmp;
 	int min;
+	tmp = 0;
 	min = this->longestSpan(); 
-	sort(this->_vec->begin(), this->_vec->end());
 	for (unsigned i=0; i < this->_vec->size(); i++){
 		for (unsigned j=i+1; j < this->_vec->size(); j++){
-			std::cout << "{" << i << "}" << "{" << j << "}" << this->_vec->at(i) << "-" << this->_vec->at(j) << "=" << min << std::endl;
-			if (this->_vec->at(i) - this->_vec->at(j) < min){ 
-				std::cout << min << std::endl;
-				if (this->_vec->at(i) - this->_vec->at(j) > 0){
-					min = this->_vec->at(i) - this->_vec->at(j);
-				}
-			}
+			tmp = this->_vec->at(i) - this->_vec->at(j);
+			if (tmp < 0)
+				tmp = tmp * -1;
+			if (tmp < min)
+				min = tmp;
 		}
 	}
 	return min;
 }
 
 std::ostream & operator<<(std::ostream & o, Span const & ref){
-	std::cout << "Vector contains " << ref.getN() << " elementes max" << std::endl;
+	std::cout << "Vector capacity " << ref.getN() << " elementes max" << std::endl;
 	std::cout << GREEN << "####################" << RESET << std::endl;
 	  for (unsigned i=0; i < ref.getVec().size(); i++)
 		std::cout << "[" << i << "]" << ref.getVec().at(i) << std::endl;
 	std::cout << RED << "####################" << RESET << std::endl;
 	return (o);
 }
+
+			/* std::cout << "{" << i << "}" << "{" << j << "}" << this->_vec->at(i) << "-" << this->_vec->at(j) << "=" << min << std::endl; */
