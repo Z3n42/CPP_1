e29aef4a483f5639b35c4aace8d7627a231c6891
ingonzal <zen42.ikas@gmail.com>
2023-07-27 18:57:17 +0200

    Cpp09 add csv

--- cursus/6Ring/cpp/cpp09/ex00/main.cpp
+++ cursus/6Ring/cpp/cpp09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ingonzal <ingonzal@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 18:44:48 by ingonzal          #+#    #+#             */
-/*   Updated: 2023/07/24 19:01:18 by ingonzal         ###   ########.fr       */
+/*   Updated: 2023/07/27 18:09:20 by ingonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 int main(void){
 	std::string line;
-	std::ifstream myfile ("example.txt");
+	std::ifstream myfile ("data.csv");
 	if (myfile.is_open()){
 		while (std::getline(myfile,line)){
 			std::cout << line << '\n';
