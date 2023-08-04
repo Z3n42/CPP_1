f5100412abc51e93f161a449a1d10167d5ef9381
Iñigo Gonzalez Portela <zen42.ikas@gmail.com>
2023-08-04 18:53:12 +0200

    CPP08_pass

--- cursus/6Ring/cpp/cpp09/ex00/main.cpp
+++ cursus/6Ring/cpp/cpp09/ex00/main.cpp
@@ -6,13 +6,19 @@
 /*   By: ingonzal <ingonzal@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 18:44:48 by ingonzal          #+#    #+#             */
-/*   Updated: 2023/08/03 19:23:48 by ingonzal         ###   ########.fr       */
+/*   Updated: 2023/08/04 18:50:22 by ingonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-int main(void){
+int main(int argc, char** argv){
+	if (argc != 2){
+		initBtc(argv);
+	}
+	else
+		std::cout << "invalid arguments" << std::endl;
+
 	BitcoinExchange btc;
 	btc.addData("Data.csv");
 	btc.addInput("input.txt");
