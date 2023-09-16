8143ef74d85c4fd1df6ae358b6e8b2f844295db2
ingonzal <zen42.ikas@gmail.com>
2023-09-16 20:23:44 +0200

    certbot try

--- cursus/6Ring/Inception/init-letsencrypt.sh
+++ cursus/6Ring/Inception/init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(ingonzal.42.fr www.ingonzal.42.fr)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="ingonzal@student.42urduliz.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
