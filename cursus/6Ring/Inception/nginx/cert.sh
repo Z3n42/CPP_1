e5874a39a65aeb50bbb9a660e9c8e620dc05b6c6
ingonzal <zen42.ikas@gmail.com>
2023-09-17 20:21:27 +0200

    nginx ssl, cert, entrypoint done

--- cursus/6Ring/Inception/nginx/cert.sh
+++ cursus/6Ring/Inception/nginx/cert.sh
@@ -10,6 +10,82 @@
 #    Updated: 2023/09/16 18:57:37 by ingonzal         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+#!/bin/bash
+
+# Directorio donde se almacenarán los certificados
+cert_dir="/tmp"
+# mkdir $cert_dir
+
+# Comprobar si ya existen certificados
+if [ ! -f "$cert_dir/ingonzal.42.fr.crt" ] && [ ! -f "$cert_dir/ingonzal.42.fr.key" ]; then
+    echo "Creando certificados SSL auto-firmados..."
+
+    # Generar certificados SSL auto-firmados
+    openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 \
+        -subj "/C=US/ST=California/L=San Francisco/O=Example/CN=localhost" \
+        -keyout "$cert_dir/ingonzal.42.fr.key" -out "$cert_dir/ingonzal.42.fr.crt" &&
+
+    echo "Certificados creados." &&
+
+    # Mover certificados a su ubicación final
+    mv $cert_dir/ingonzal.42.fr.crt /etc/ssl/certs/ &&
+    mv $cert_dir/ingonzal.42.fr.key /etc/ssl/private/ &&
+
+    echo "Certificados movidos a su ubicación final."
+else
+    echo "Certificados ya existen en $cert_dir."
+rm $cert_dir/ingonzal.42.fr.key &&  $cert_dir ingonzal.42.fr.crt
+fi
+
+
+# Configurar NGINX para usar los certificados
+cat <<EOF > /etc/nginx/conf.d/default.conf
+server {
+    listen 	443 ssl;
+    listen  [::]:443;
+    server_name  localhost;
+
+	ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
+	ssl_ciphers ALL:!aNULL:!ADH:!eNULL:!LOW:!EXP:RC4+RSA:+HIGH:+MEDIUM;
+
+    keepalive_timeout 75 75;
+
+    ssl_certificate /etc/ssl/certs/ingonzal.42.fr.crt;
+    ssl_certificate_key /etc/ssl/private/ingonzal.42.fr.key;
+	ssl_session_timeout  5m;
+
+    add_header Strict-Transport-Security "max-age=7200";
+
+    location / {
+        root /usr/share/nginx/html;
+        index index.html;
+    }
+
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   /usr/share/nginx/html;
+    }
+
+    location ~ \.php$ {
+        root           html;
+        fastcgi_pass   127.0.0.1:9000;
+        fastcgi_index  index.php;
+        fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
+        include        fastcgi_params;
+	}
+
+}
+EOF
+
+echo "Recargando NGINX..."
+nginx -s reload
+
+# Manejar señales para un apagado limpio
+# trap 'nginx -s stop' SIGTERM
+
+# Iniciar el servidor NGINX
+echo "Iniciando el servidor NGINX..."
+nginx -g 'daemon off;'
 
 #!/bin/bash
 #
@@ -27,53 +103,53 @@
 #
 # The certbot-auto program logs to /var/log/letsencrypt.
 #
- 
-set -o nounset
-set -o errexit
- 
+
+# set -o nounset
+# set -o errexit
+
 # May or may not have HOME set, and this drops stuff into ~/.local.
-export HOME="/root"
-export PATH="${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
- 
+# export HOME="/root"
+# export PATH="${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
 # No package install yet.
 # wget https://dl.eff.org/certbot-auto
 # chmod a+x certbot-auto
 # mv certbot-auto /usr/local/bin
- 
+
 # Install the dependencies.
 # certbot-auto --noninteractive --os-packages-only
- 
+
 # Set up config file.
 # mkdir -p /etc/letsencrypt
 # cat > /etc/letsencrypt/cli.ini <<EOF
 # # Uncomment to use the staging/testing server - avoids rate limiting.
 # # server = https://acme-staging.api.letsencrypt.org/directory
- 
+
 # # Use a 4096 bit RSA key instead of 2048.
 # rsa-key-size = 4096
- 
+
 # # Set email and domains.
 # email = admin@example.com
 # domains = example.com, www.example.com
- 
+
 # # Text interface.
 # text = True
 # # No prompts.
 # non-interactive = True
 # # Suppress the Terms of Service agreement interaction.
 # agree-tos = True
- 
+
 # # Use the webroot authenticator.
 # authenticator = webroot
 # webroot-path = /var/www/html
 # EOF
- 
+
 # Obtain cert.
-certbot --nginx -d example.com -d www.example.com
- 
+# certbot --nginx -d example.com -d www.example.com
+
 # Set up daily cron job.
 #CRON_SCRIPT="/etc/cron.daily/certbot-renew"
- 
+
 #cat > "${CRON_SCRIPT}" <<EOF
 ##!/bin/bash
 ##
@@ -82,14 +158,14 @@ certbot --nginx -d example.com -d www.example.com
 ##
 ## This reads the standard /etc/letsencrypt/cli.ini.
 ##
- 
+
 ## May or may not have HOME set, and this drops stuff into ~/.local.
 #export HOME="/root"
 ## PATH is never what you want it it to be in cron.
 #export PATH="\${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
- 
+
 #certbot-auto --no-self-upgrade certonly
- 
+
 ## If the cert updated, we need to update the services using it. E.g.:
 #if service --status-all | grep -Fq 'apache2'; then
 #  service apache2 reload
