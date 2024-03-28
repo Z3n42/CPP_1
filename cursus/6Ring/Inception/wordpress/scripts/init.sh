e82682473f94bc6eed93d92db8e6602a94e4bb6a
Iñigo Gonzalez Portela <zen42.ikas@gmail.com>
2024-03-28 12:30:42 +0100

    bad env

--- cursus/6Ring/Inception/wordpress/scripts/init.sh
+++ cursus/6Ring/Inception/wordpress/scripts/init.sh
@@ -1,31 +1,70 @@
 #!/bin/bash
 
-# installing of wp-cli
-curl -q -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /opt/wp-cli.phar
-# php
-if [ ! -d /run/php ]; then                                                                                                                                                                     
-        mkdir /run/php                                                                                                                                                                         
-fi                                                                                                                                                                                             
-#wordpress
-if [ ! -d /var/www/Inception-website ]; then
-        mkdir /var/www/Inception-website
-	chmod -R 777 /var/www/Inception-website
+# Enable the shell option to exit immediately if a pipeline returns a non-zero status
+set -e
+set -o pipefail
+
+# Load environment variables for configuration
+# source ../.env
+
+# Install wp-cli
+curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o /usr/local/bin/wp
+chmod +x /usr/local/bin/wp
+
+# Create directory for PHP if it doesn't exist
+if [ ! -d /run/php ]; then
+    mkdir -p /run/php
 fi
 
-if [ ! -f /var/www/Inception-website/wp-config.php ]; then
-        /opt/wp-cli.phar core download --path=/var/www/Inception-website --allow-root
-        /opt/wp-cli.phar config create --path=/var/www/Inception-website --dbname=$DB_NAME --dbhost=$DB_HOST --dbuser=$USER --dbpass=$USER_PASSWORD --allow-root
-        /opt/wp-cli.phar core install --path=/var/www/Inception-website --url=https://yakhoudr.42.fr --title=Inception --admin_user=$WORD_ADMIN --admin_password=$WORD_ADMIN_PASSWORD --admin_email=$WORD_ADMIN_EMAIL --allow-root                                                                                                                                                                          
-        /opt/wp-cli.phar user create $WORD_USER $WORD_USER_EMAIL --role=$WORD_USER_ROLE --user_pass=$WORD_USER_PASS --path=/var/www/Inception-website --allow-root                                           
-else                                                                                                                                                                                           
-        echo "Wordpress is already installed."                                                                                                                                                 
+# Create directory for WordPress if it doesn't exist
+WP_DIR="/var/www/Inception-website"
+if [ ! -d "$WP_DIR" ]; then
+    mkdir -p "$WP_DIR"
+    # Assign appropriate permissions
+    chown www-data:www-data "$WP_DIR"
+    find "$WP_DIR" -type d -exec chmod 755 {} \;
+    find "$WP_DIR" -type f -exec chmod 644 {} \;
 fi
-#php-fpm                                                                                                                                                                                       
-echo "php-fpm is now running."
+
+# # Wait for MariaDB to be ready
+# echo "Waiting for MariaDB to be ready..."
+# until mysqladmin ping -h"${WORDPRESS_DB_HOST}" --silent; do
+#     echo "MariaDB is not up yet... waiting"
+#     sleep 1
+# done
+sleep 30
+
+# Check if WordPress core files are present
+if [ ! -f "$WP_DIR/wp-config.php" ]; then
+    # Install WordPress if not already installed
+    wp core download --path="$WP_DIR" --allow-root
+    wp config create --path="$WP_DIR" --dbname="$MYSQL_DATABASE" --dbhost="$WORDPRESS_DB_HOST" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
+    wp core install --path="$WP_DIR" --url="$WORDPRESS_URL" --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --allow-root
+    wp user create "$WORDPRESS_USER" "$WORDPRESS_USER_EMAIL" --role="$WORDPRESS_USER_ROLE" --user_pass="$WORDPRESS_USER_PASSWORD" --path="$WP_DIR" --allow-root
+    echo "WordPress installed"
+else
+    echo "WordPress core files are already present at $WP_DIR."
+fi
+
+# Add debugging configuration to wp-config.php if not present
+WP_CONFIG="$WP_DIR/wp-config.php"
+if [ -f "$WP_CONFIG" ]; then
+    echo "WordPress configured"
+    # Ensure that the debugging lines do not exist before adding them
+    if ! grep -q "define( 'WP_DEBUG'" "$WP_CONFIG"; then
+        echo "Adding debugging configuration to $WP_CONFIG"
+        sed -i "/^\/\* That's all, stop editing! Happy publishing. \*\//i\
+define( 'WP_DEBUG', true );\
+define( 'WP_DEBUG_LOG', true );\
+define( 'WP_DEBUG_DISPLAY', false );\
+@ini_set( 'display_errors', 0 );\
+" "$WP_CONFIG"
+    fi
+fi
+
+# Start php-fpm
+echo "Starting php-fpm..."
 exec php-fpm7.4 -F
-
 
 # #!/bin/bash
 
@@ -33,54 +72,54 @@ exec php-fpm7.4 -F
 # set -e
 # set -o pipefail
 
-# # Create necessary directories for MariaDB
-# mkdir -p /var/run/mysqld /run/mysqld
-# chown -R mysql:mysql /var/run/mysqld /run/mysqld
+# # Load environment variables for configuration
+# # source ../.env
 
-# # Initialize the database if not present
-# if [ ! -d /var/lib/mysql/mysql ]; then
-#   echo "Initializing the database..."
-#   mysql_install_db --user=root
+# # Install wp-cli
+# curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o /usr/local/bin/wp
+# chmod +x /usr/local/bin/wp
+
+# # Create directory for PHP if it doesn't exist
+# if [ ! -d /run/php ]; then
+#     mkdir -p /run/php
 # fi
 
-# # Start MariaDB service in the background
-# echo "Starting MariaDB service..."
-# service mariadb start
-
-# # Wait until MariaDB server is online
-# echo "Waiting for MariaDB server to be online..."
-# while ! mysqladmin ping -hlocalhost --silent 2>/dev/null; do
-# 	 sleep 1
-# done
-
-# # Execute initial SQL queries
-# if [[ -f /scripts/init.sql ]]; then
-#   echo "Executing initial SQL queries..."
-#   # Create a temporary SQL file with environment variables substituted
-#   envsubst < /scripts/init.sql > /tmp/init.sql
-#   # Execute the SQL queries in the temporary file
-#   mysql -u root -p${MYSQL_ROOT_PASSWORD} < /tmp/init.sql
-#   # Remove the temporary file
-#   rm /tmp/init.sql
+# # Create directory for WordPress if it doesn't exist
+# WP_DIR="/var/www/Inception-website"
+# if [ ! -d "$WP_DIR" ]; then
+#     mkdir -p "$WP_DIR"
+#     # Assign appropriate permissions
+#     chown www-data:www-data "$WP_DIR"
+#     find "$WP_DIR" -type d -exec chmod 755 {} \;
+#     find "$WP_DIR" -type f -exec chmod 644 {} \;
 # fi
 
-# # Stop MariaDB service
-# echo "Stopping MariaDB service..."
-# service mariadb stop
+# # Check if WordPress core files are present
+# if [ ! -f "$WP_DIR/wp-config.php" ] && [ ! -f "$WP_DIR/wp-settings.php" ]; then
+#     # Install WordPress if not already installed
+#     wp core download --path="$WP_DIR" --allow-root
+#     wp config create --path="$WP_DIR" --dbname="$MYSQL_DATABASE" --dbhost="$WORDPRESS_DB_HOST" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
+#     wp core install --path="$WP_DIR" --url="$WORDPRESS_URL" --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN_USER" --admin_password="$WORDPRESS_ADMIN_PASSWORD" --admin_email="$WORDPRESS_ADMIN_EMAIL" --allow-root
+#     wp user create "$WORDPRESS_USER" "$WORDPRESS_USER_EMAIL" --role="$WORDPRESS_USER_ROLE" --user_pass="$WORDPRESS_USER_PASSWORD" --path="$WP_DIR" --allow-root
+# else
+#     echo "WordPress core files are already present at $WP_DIR."
+# fi
 
-# # Wait until MariaDB server has stopped
-# echo "Waiting for MariaDB server to stop..."
-# while mysqladmin ping -hlocalhost --silent 2>/dev/null; do
-# 	echo "Waiting..."
-# 	sleep 1
-# done
+# # Add debugging configuration to wp-config.php if not present
+# WP_CONFIG="$WP_DIR/wp-config.php"
+# if [ -f "$WP_CONFIG" ]; then
+#     # Ensure that the debugging lines do not exist before adding them
+#     if ! grep -q "define( 'WP_DEBUG'" "$WP_CONFIG"; then
+#         echo "Adding debugging configuration to $WP_CONFIG"
+#         sed -i "/^\/\* That's all, stop editing! Happy publishing. \*\//i\
+# define( 'WP_DEBUG', true );\
+# define( 'WP_DEBUG_LOG', true );\
+# define( 'WP_DEBUG_DISPLAY', false );\
+# @ini_set( 'display_errors', 0 );\
+# " "$WP_CONFIG"
+#     fi
+# fi
 
-
-# # Trap SIGTERM and SIGINT signals and gracefully exit the script
-# trap 'echo "Caught signal, stopping MariaDB service..."; service mariadb stop; exit 0' SIGTERM SIGINT
-
-# # Start MariaDB server in the foreground
-# echo "Starting MariaDB server in the foreground..."
-# # killall mysqld
-# exec mysqld
-
+# # Start php-fpm
+# echo "Starting php-fpm..."
+# exec php-fpm7.4 -F
